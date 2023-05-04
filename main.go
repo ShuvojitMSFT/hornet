34169c0fa1050a348bdc960f24734e6b11da80ce
ShuvojitMSFT <51408571+ShuvojitMSFT@users.noreply.github.com>
2023-05-04 19:31:06 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,21 +2,33 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
+    "encoding/json"
+    "fmt"
+    "log"
+    "net/http"
+    "time"
 
-	_ "github.com/lib/pq"
+    _ "github.com/lib/pq"
+    "github.com/gorilla/mux"
+    "github.com/gorilla/context"
 )
 
 
-type hornetUser struct {
+type hornetUserSignin struct {
 	ID       int    `json:"id"`
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+
+type hornetUserSignup struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+
 
 func main() {
 
@@ -38,7 +50,7 @@ func main() {
 	
 	http.HandleFunc("/signup", func(w http.ResponseWriter, r *http.Request) {
 		// Parse the request body into a User struct
-		var user hornetUser
+		var user hornetUserSignup
 		err := json.NewDecoder(r.Body).Decode(&user)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
@@ -72,7 +84,7 @@ func main() {
 	
 	http.HandleFunc("/signin", func(w http.ResponseWriter, r *http.Request) {
 		// Parse the request body into a hornetUser struct
-		var user hornetUser
+		var user hornetUserSignin
 		err := json.NewDecoder(r.Body).Decode(&user)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
@@ -82,26 +94,34 @@ func main() {
 		// Query the database for the user's email and password
 		row := db.QueryRow("SELECT id, name FROM users WHERE email = $1 AND password = $2", user.Email, user.Password)
 
-		// Scan the row into a User struct
-		var matchedUser User
+		
+		var matchedUser hornetUserSignin
 		err = row.Scan(&matchedUser.ID, &matchedUser.Name)
 		if err != nil {
 			http.Error(w, "Invalid email or password", http.StatusUnauthorized)
 			return
 		}
 
-		// Respond with the matched user's ID and name
+		
 		response := struct {
 			ID   int    `json:"id"`
 			Name string `json:"name"`
 		}{matchedUser.ID, matchedUser.Name}
 		json.NewEncoder(w).Encode(response)
 	})
+	
+	
 
+
+
+// This part is if the server is on the local host. I am using a GCP instance of postgre
+
+/*		
 	// Start the server
 	log.Println("Server listening on 5432")
 	err = http.ListenAndServe("34.72.216.95:5432", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+	*/
 }
